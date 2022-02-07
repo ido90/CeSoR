731fb6d71594114edca07f50f5c60223afd8395f
ido90 <idogreenberg90@gmail.com>
2022-02-08 00:22:59 +0200

    Agents: support CNN & cuda

--- Agents.py
+++ Agents.py
@@ -10,75 +10,28 @@ import torch.nn.functional as F
 
 MODELS_PATH = 'models'
 
+
 class NN(nn.Module):
-    def __init__(self, mid_sizes=None, state_dim=4, act_dim=2, lstm=True,
-                 dropout=0.0, eps_factor=0, train_hparams=None, title='',
-                 pretrained_filename=None):
+    def __init__(self, state_dim=4, act_dim=2, eps_factor=0, train_hparams=None,
+                 device='cpu', title='', pretrained_filename=None):
         super(NN, self).__init__()
         self.title = title
-        self.device = 'cpu'
-        self.lstm = lstm
+        self.device = device
         self.state_dim = state_dim
         self.act_dim = act_dim
-        if mid_sizes is None:
-            mid_sizes = (16,) if self.lstm else (32,32)
-        self.mid_sizes = mid_sizes
         self.eps_factor = eps_factor
         self.train_hparams = train_hparams if isinstance(train_hparams, dict) else None
         self.pretrained_filename = pretrained_filename
 
-        sizes = [self.state_dim] + list(mid_sizes)
-        layer = nn.LSTMCell if self.lstm else nn.Linear
-
-        self.layers = nn.ModuleList([layer(a, b) for a,b in zip(sizes[:-1],sizes[1:])])
-        self.drop_p = dropout
-        self.dropouts = None
-        if self.drop_p:
-            self.dropouts = [nn.Dropout(p=dropout) for _ in range(len(mid_sizes))]
-        self.head = nn.Linear(mid_sizes[-1], self.act_dim)
-
-        self.h, self.c = None, None
-        self.init_state()
-
         self.n_updates = 0
 
     def init_state(self):
-        if self.lstm:
-            # hidden state
-            self.h = [torch.zeros(1,m).to(self.device)
-                      for m in self.mid_sizes]
-            # memory cell
-            self.c = [torch.zeros(1,m).to(self.device)
-                      for m in self.mid_sizes]
-
-    def forward(self, x, T=1):
-        if T==0: T=1
-
-        if self.lstm:
-            for j, layer in enumerate(self.layers):
-                self.h[j], self.c[j] = layer(
-                    self.h[j-1] if j>0 else x, (self.h[j], self.c[j]))
-            x = self.h[-1]
-        else:
-            for j, layer in enumerate(self.layers):
-                x = layer(x)
-                if self.drop_p:
-                    x = self.dropouts[j](x)
-                x = F.relu(x)
-
-        action_scores = self.head(x)
-        return F.softmax(action_scores/T, dim=1)
-
-    def sign(self, to_print=True):
-        cls = 'LSTM' if self.lstm else 'Fully-connected network'
-        sign = f'Solver: {cls}.'
-        if to_print:
-            print(sign)
-        return sign
+        pass
 
     def get_params_hash(self):
         return hex(hash(torch.cat(
-            [par.flatten() for par in self.parameters()]).detach().numpy().tostring()))
+            [par.flatten() for par in self.parameters()]
+        ).cpu().detach().numpy().tostring()))
 
     def save(self, filename=None):
         if filename is None: filename = self.title
@@ -117,3 +70,75 @@ class NN(nn.Module):
             print(f'action = {action:d}\t({choice:s})')
 
         return action, probs[0,[action]].log(), choice
+
+
+class FC(NN):
+    def __init__(self, mid_sizes=None, state_dim=4, act_dim=2, lstm=False,
+                 dropout=0.0, **kwargs):
+        super(FC, self).__init__(state_dim, act_dim, **kwargs)
+
+        self.lstm = lstm
+        if mid_sizes is None:
+            mid_sizes = (16,) if self.lstm else (32,32)
+        self.mid_sizes = mid_sizes
+        sizes = [self.state_dim] + list(mid_sizes)
+        layer = nn.LSTMCell if self.lstm else nn.Linear
+
+        self.layers = nn.ModuleList([layer(a, b) for a,b in zip(sizes[:-1],sizes[1:])])
+        self.drop_p = dropout
+        self.dropouts = None
+        if self.drop_p:
+            self.dropouts = [nn.Dropout(p=dropout) for _ in range(len(mid_sizes))]
+        self.head = nn.Linear(mid_sizes[-1], self.act_dim)
+
+        self.h, self.c = None, None
+        self.init_state()
+
+    def init_state(self):
+        if self.lstm:
+            # hidden state
+            self.h = [torch.zeros(1,m).to(self.device)
+                      for m in self.mid_sizes]
+            # memory cell
+            self.c = [torch.zeros(1,m).to(self.device)
+                      for m in self.mid_sizes]
+
+    def forward(self, x, T=1):
+        if T==0: T=1
+
+        if self.lstm:
+            for j, layer in enumerate(self.layers):
+                self.h[j], self.c[j] = layer(
+                    self.h[j-1] if j>0 else x, (self.h[j], self.c[j]))
+            x = self.h[-1]
+        else:
+            for j, layer in enumerate(self.layers):
+                x = layer(x)
+                if self.drop_p:
+                    x = self.dropouts[j](x)
+                x = F.relu(x)
+
+        action_scores = self.head(x)
+        return F.softmax(action_scores/T, dim=1)
+
+
+class CNN(NN):
+    def __init__(self, mid_sizes=(16,16), state_dim=8, act_dim=4, in_channels=3,
+                 **kwargs):
+        super(CNN, self).__init__(state_dim, act_dim, **kwargs)
+
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, mid_sizes[0], kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(mid_sizes[0], mid_sizes[1], kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(self.state_dim//4, self.state_dim//4),
+            nn.Flatten(),
+            nn.Linear(mid_sizes[1]*4*4, self.act_dim)
+        ).to(self.device)
+
+    def forward(self, x, T=1):
+        if T==0: T=1
+        x = x.to(self.device)
+        action_scores = self.model(x)
+        return F.softmax(action_scores/T, dim=1).cpu()
